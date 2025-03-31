7e4497c3b983667d05b54b7004d0bcc5ea8bf69b
William Jelgren <william@jelgren.dk>
2025-03-31 10:17:38 +0200

    Initial commit

---
+++ src/main/java/dk/zealandcs/racekatteklubben/RacekatteklubbenApplication.java
@@ -0,0 +1,13 @@
+package dk.zealandcs.racekatteklubben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacekatteklubbenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RacekatteklubbenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/zealandcs/racekatteklubben/RacekatteklubbenApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.zealandcs.racekatteklubben;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacekatteklubbenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
