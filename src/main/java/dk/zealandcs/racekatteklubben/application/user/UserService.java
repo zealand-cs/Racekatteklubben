cc1d00affdac6e4eab71c358762c412ca21fdd56
William Jelgren <william@jelgren.dk>
2025-04-01 21:31:56 +0200

    Very simple list of users

--- src/main/java/dk/zealandcs/racekatteklubben/application/user/UserService.java
+++ src/main/java/dk/zealandcs/racekatteklubben/application/user/UserService.java
@@ -7,6 +7,7 @@ import dk.zealandcs.racekatteklubben.infrastructure.user.UserWriteException;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,6 +15,11 @@ public class UserService implements IUserService {
     private final IUserRepository userRepository;
 
     UserService(IUserRepository repo) { this.userRepository = repo; }
+
+    @Override
+    public List<User> allUsers() {
+        return userRepository.findAll();
+    }
 
     @Override
     public Optional<User> getUser(int id) {
